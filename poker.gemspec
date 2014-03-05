d7e38914df266026a33e8bdc3ac17cb4cdc67531
Wojtek Mach <wojtek@wojtekmach.pl>
2014-03-05 18:57:56 +0100

    Add simplecov & coveralls

--- poker.gemspec
+++ poker.gemspec
@@ -22,4 +22,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency 'rake'
   s.add_development_dependency 'minitest', '~> 5.0'
+  s.add_development_dependency 'coveralls'
+  s.add_development_dependency 'simplecov'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,24 @@
 require 'minitest/autorun'
+
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter,
+  ]
+
+  SimpleCov.start do
+    command_name 'test'
+
+    add_filter 'test'
+    add_filter 'examples'
+    add_filter 'ext'
+
+    minimum_coverage 100
+  end
+end
+
 require 'poker'
